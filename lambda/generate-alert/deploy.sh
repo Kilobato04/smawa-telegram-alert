93cb88de493f5ee1900d46f57aed5e5df3e06eb4
Kilobato04 <134810631+Kilobato04@users.noreply.github.com>
2026-08-05 12:47:55 -0600

    Update deploy.sh

--- lambda/generate-alert/deploy.sh
+++ lambda/generate-alert/deploy.sh
@@ -1,1 +1,8 @@
-xx
+#!/bin/bash
+echo "Empaquetando Lambda..."
+npm install
+zip -r smawa-lambda.zip . -x "*.git*" "deploy.sh" "test-local.js"
+echo "Subiendo a AWS Lambda..."
+# Reemplaza 'SmawaAlertFunction' por el nombre real de tu función en AWS
+aws lambda update-function-code --function-name SmawaAlertFunction --zip-file fileb://smawa-lambda.zip
+echo "Despliegue completado."
